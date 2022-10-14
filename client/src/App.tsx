540e70b6d03ca6ae6237331c18e0c37d696196ad
celiovjunior <celio_cjr@hotmail.com>
2022-10-14 02:17:56 -0300

    setting the logic about the todo been complete or not, made the proper functions to mutate state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
-import { Box } from '@mantine/core'
+import { Box, List, ThemeIcon } from '@mantine/core'
+import { CheckCircleFillIcon } from '@primer/octicons-react';
 import useSWR from 'swr'
-import './App.css'
 import AddTodo from './components/AddTodo'
+
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
 
 export const ENDPOINT = 'http://localhost:4000'
 
@@ -9,12 +16,33 @@ const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then(r => r.json())
 
 function App() {
 
-  const { data, mutate } = useSWR('api/todos', fetcher)
+  const { data, mutate } = useSWR<Todo[]>('api/todos', fetcher)
 
   return(
-    <Box>
-      {JSON.stringify(data)}
-      <AddTodo />
+    <Box
+      sx={(theme) => ({
+        padding: '2rem',
+        width: '100%',
+        maxWidth: '40rem',
+        margin: '0 auto'
+      })}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return <List.Item key={`todo_list__${todo.id}`}
+            icon={
+              todo.done ? (<ThemeIcon color="teal" size={24} radius="xl">
+                <CheckCircleFillIcon size={20} />
+              </ThemeIcon>) : (<ThemeIcon color="gray" size={24} radius="xl">
+                <CheckCircleFillIcon size={20} />
+              </ThemeIcon>)
+            }
+          >
+            {todo.title}
+          </List.Item>
+        })}
+      </List>
+      <AddTodo mutate={mutate} />
     </Box>
   )
 }
--- client/src/components/AddTodo.tsx
+++ client/src/components/AddTodo.tsx
@@ -1,8 +1,10 @@
-import { Button, Group, Modal } from "@mantine/core"
+import { Button, Group, Modal, Textarea, TextInput } from "@mantine/core"
 import { useForm } from "@mantine/form"
 import { useState } from "react"
+import { KeyedMutator } from "swr"
+import { ENDPOINT, Todo } from "../App"
 
-export default function AddTodo() {
+export default function AddTodo({ mutate }: {mutate:KeyedMutator<Todo[]>}) {
     const [open, setOpen] = useState(false)
 
     const form = useForm({
@@ -12,14 +14,47 @@ export default function AddTodo() {
         }
     })
 
+    async function createTodo(values: {title: string, body: string}) {
+        const updated = await fetch(`${ENDPOINT}/api/todos`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(values)
+        }).then(r => r.json());
+
+        mutate(updated)
+
+        form.reset()
+        setOpen(false)
+    }
+
     return(
         <>
             <Modal opened={open}
                 onClose={() => setOpen(false)}
                 title="Create todo"
             >
-                Text
+                <form onSubmit={form.onSubmit(createTodo)}>
+                    <TextInput 
+                        required
+                        mb={12}
+                        label="Todo"
+                        placeholder="What do you want to do?"
+                        {...form.getInputProps("title")}
+                    />
+                    <Textarea 
+                        required
+                        mb={12}
+                        label="Text"
+                        placeholder="Write a little description"
+                        {...form.getInputProps("body")}
+                    />
+
+                    <Button type="submit">Create Todo</Button>
+                </form>
             </Modal>
+
 
             <Group position="center">
                 <Button fullWidth mb={12} onClick={() => setOpen(true)}>
